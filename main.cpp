750c3575fb1895d31324676829b2054122c2be2b
Juan Mo <juan.mo.song@gmail.com>
2018-07-21 21:45:19 +0200

    template specialization variant.

--- main.cpp
+++ main.cpp
@@ -13,17 +13,27 @@ class Cart
     
     Cart() : appleBasket(10), pearBasket(10), tomatoBasket(10), potatoBasket(10) { }
     
-    void addToCart(Apple  apple)  { appleBasket.push_back(apple);    }
-    void addToCart(Pear   pear)   { pearBasket.push_back(pear);      }
-    void addToCart(Tomato tomato) { tomatoBasket.push_back(tomato); }
-    void addToCart(Potato potato) { potatoBasket.push_back(potato); }
+
+	template<typename T>
+	void addToCart(T item){
+		getBasket<T>().push_back(item);
+	}
     
     private:
     std::vector<Apple>          appleBasket;
     std::vector<Pear>           pearBasket;
     std::vector<Tomato>         tomatoBasket;
     std::vector<Potato>         potatoBasket;
+
+	template<typename T>
+	inline std::vector<T>& getBasket();
+
 };
+
+template<typename T> std::vector<T>& Cart::getBasket() { return appleBasket; }
+template<> std::vector<Pear>& Cart::getBasket() { return pearBasket;}
+template<> std::vector<Tomato>& Cart::getBasket(){ return tomatoBasket; }
+template<> std::vector<Potato>& Cart::getBasket(){	return potatoBasket; }
 
 
 int main()
